8b841685eed02af3d3c908e0a0b20b89fc9c8813
Fernando Lopez Casillas <flcasillas@gmail.com>
2019-08-30 12:22:20 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShellComponent } from './core/shell/shell.component';
 
 
 const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    data: { title: 'Home' },
+
+    children: [
+      {
+        path: 'base',
+        loadChildren: () => import('./views/base/base.module').then(m => m.BaseModule)
+      },
+    ]
+  }
 
 ];
 
